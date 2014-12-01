07cce7f182078e5bfcefe3999501a0546a01cc21
root <root@bennette-vm.(none)>
2014-12-02 00:48:24 +0100

    sql dump

--- translation.sql
+++ translation.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le : Lun 01 Décembre 2014 à 00:32
+-- Généré le : Mar 02 Décembre 2014 à 00:47
 -- Version du serveur: 5.5.40
 -- Version de PHP: 5.5.18-1+deb.sury.org~precise+1
 
@@ -38,9 +38,9 @@ CREATE TABLE IF NOT EXISTS `Difficulty` (
 --
 
 INSERT INTO `Difficulty` (`id`, `name`, `nb`) VALUES
-(1, 'facile', 10),
-(2, 'normal', 5),
-(3, 'difficile', 3);
+(1, 'facile', 3),
+(2, 'normal', 2),
+(3, 'difficile', 1);
 
 -- --------------------------------------------------------
 
@@ -52,14 +52,15 @@ CREATE TABLE IF NOT EXISTS `Game` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `Game`
 --
 
 INSERT INTO `Game` (`id`, `name`) VALUES
-(1, 'Traduire');
+(1, 'Traduction'),
+(2, 'Verbes irréguliers');
 
 -- --------------------------------------------------------
 
@@ -3000,9 +3001,7 @@ INSERT INTO `Import` (`french`, `english`, `categoryId`) VALUES
 ('zone rurale', 'rural area, rural zone', 3),
 ('zone très peuplée', 'highly populated area, highly populated zone', 3),
 ('zone urbaine', 'urban area, urban zone', 3),
-('Zut !', 'Damn it!', 18),
-(' ', ' ', 18),
-(' ', 'dark (~ hair)', 25);
+('Zut !', 'Damn it!', 18);
 
 -- --------------------------------------------------------
 
@@ -3056,6 +3055,202 @@ INSERT INTO `ImportCategory` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `ImportIrregular`
+--
+
+CREATE TABLE IF NOT EXISTS `ImportIrregular` (
+  `english` varchar(255) NOT NULL,
+  `preterit` varchar(255) NOT NULL,
+  `past` varchar(255) NOT NULL,
+  `french` varchar(255) NOT NULL,
+  UNIQUE KEY `english` (`english`,`preterit`,`past`,`french`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `ImportIrregular`
+--
+
+INSERT INTO `ImportIrregular` (`english`, `preterit`, `past`, `french`) VALUES
+('abide', 'abode', 'abode', 'respecter / se conformer à'),
+('arise', 'arose', 'arisen', 'survenir'),
+('awake', 'awoke', 'awoken', 'se réveiller'),
+('be', 'was, were', 'been', 'être'),
+('bear', 'bore', 'borne / born', 'porter / supporter / naître'),
+('beat', 'beat', 'beaten', 'battre'),
+('become', 'became', 'become', 'devenir'),
+('beget', 'begat / begot', 'begotten', 'engendrer'),
+('begin', 'began', 'begun', 'commencer'),
+('bend', 'bent', 'bent', 'plier / se courber'),
+('bereave', 'bereft / bereaved', 'bereft / bereaved', 'déposséder / priver'),
+('bet', 'bet', 'bet', 'parier'),
+('bid', 'bid / bade', 'bid / bidden', 'offrir'),
+('bleed', 'bled', 'bled', 'saigner'),
+('blow', 'blew', 'blown', 'souffler / gonfler'),
+('break', 'broke', 'broken', 'casser'),
+('breed', 'bred', 'bred', 'élever (des animaux)'),
+('bring', 'brought', 'brought', 'apporter'),
+('broadcast', 'broadcast', 'broadcast', 'diffuser / émettre'),
+('build', 'built', 'built', 'construire'),
+('burn', 'burnt / burned', 'burnt / burned', 'brûler'),
+('burst', 'burst', 'burst', 'éclater'),
+('buy', 'bought', 'bought', 'acheter'),
+('can', 'could', 'could', 'pouvoir'),
+('cast', 'cast', 'cast', 'jeter / distribuer (rôles)'),
+('catch', 'caught', 'caught', 'attraper'),
+('chide', 'chid', 'chiden', 'gronder'),
+('choose', 'chose', 'chosen', 'choisir'),
+('cling', 'clung', 'clung', 's''accrocher'),
+('clothe', 'clad / clothed', 'clad / clothed', 'habiller / recouvrir'),
+('come', 'came', 'come', 'venir'),
+('cost', 'cost', 'cost', 'coûter'),
+('creep', 'crept', 'crept', 'ramper'),
+('cut', 'cut', 'cut', 'couper'),
+('deal', 'dealt', 'dealt', 'distribuer'),
+('dig', 'dug', 'dug', 'creuser'),
+('dive', 'dived', 'dived / dove', 'plonger'),
+('do', 'did', 'done', 'faire'),
+('draw', 'drew', 'drawn', 'dessiner / tirer'),
+('dream', 'dreamt / dreamed', 'dreamt / dreamed', 'rêver'),
+('drink', 'drank', 'drunk', 'boire'),
+('drive', 'drove', 'driven', 'conduire'),
+('dwell', 'dwelt', 'dwelt / dwelled', 'habiter'),
+('eat', 'ate', 'eaten', 'manger'),
+('fall', 'fell', 'fallen', 'tomber'),
+('feed', 'fed', 'fed', 'nourrir'),
+('feel', 'felt', 'felt', 'se sentir / ressentir'),
+('fight', 'fought', 'fought', 'se battre'),
+('find', 'found', 'found', 'trouver'),
+('flee', 'fled', 'fled', 's''enfuir'),
+('fling', 'flung', 'flung', 'lancer'),
+('fly', 'flew', 'flown', 'voler'),
+('forbid', 'forbade', 'forbidden', 'interdire'),
+('forecast', 'forecast', 'forecast', 'prévoir'),
+('foresee', 'foresaw', 'foreseen', 'prévoir / présentir'),
+('forget', 'forgot', 'forgotten / forgot', 'oublier'),
+('forgive', 'forgave', 'forgiven', 'pardonner'),
+('forsake', 'forsook', 'forsaken', 'abandonner'),
+('freeze', 'froze', 'frozen', 'geler'),
+('get', 'got', 'gotten / got', 'obtenir'),
+('give', 'gave', 'given', 'donner'),
+('go', 'went', 'gone', 'aller'),
+('grind', 'ground', 'ground', 'moudre / opprimer'),
+('grow', 'grew', 'grown', 'grandir / pousser'),
+('hang', 'hung', 'hung', 'tenir / pendre'),
+('have', 'had', 'had', 'avoir'),
+('hear', 'heard', 'heard', 'entendre'),
+('hide', 'hid', 'hidden', 'cacher'),
+('hit', 'hit', 'hit', 'taper / appuyer'),
+('hold', 'held', 'held', 'tenir'),
+('hurt', 'hurt', 'hurt', 'blesser'),
+('keep', 'kept', 'kept', 'garder'),
+('kneel', 'knelt / knelled', 'knelt / kneeled', 's''agenouiller'),
+('know', 'knew', 'known', 'connaître / savoir'),
+('lay', 'laid', 'laid', 'poser'),
+('lead', 'led', 'led', 'mener / guider'),
+('lean', 'leant / leaned', 'leant / leaned', 's''incliner / se pencher'),
+('leap', 'leapt / leaped', 'leapt / leaped', 'sauter / bondir'),
+('learn', 'learnt', 'learnt', 'apprendre'),
+('leave', 'left', 'left', 'laisser / quitter / partir'),
+('lend', 'lent', 'lent', 'prêter'),
+('let', 'let', 'let', 'permettre / louer'),
+('lie', 'lay', 'lain', 's''allonger'),
+('light', 'lit / lighted', 'lit / lighted', 'allumer'),
+('lose', 'lost', 'lost', 'perdre'),
+('make', 'made', 'made', 'fabriquer'),
+('mean', 'meant', 'meant', 'signifier'),
+('meet', 'met', 'met', 'rencontrer'),
+('mow', 'mowed', 'mowed / mown', 'tondre'),
+('offset', 'offset', 'offset', 'compenser'),
+('overcome', 'overcame', 'overcome', 'surmonter'),
+('partake', 'partook', 'partaken', 'prendre part à'),
+('pay', 'paid', 'paid', 'payer'),
+('plead', 'pled / pleaded', 'pled / pleaded', 'supplier / plaider'),
+('preset', 'preset', 'preset', 'programmer'),
+('prove', 'proved', 'proven / proved', 'prouver'),
+('put', 'put', 'put', 'mettre'),
+('quit', 'quit', 'quit', 'quitter'),
+('read', 'read', 'read', 'lire'),
+('relay', 'relaid', 'relaid', 'relayer'),
+('rend', 'rent', 'rent', 'déchirer'),
+('rid', 'rid', 'rid', 'débarrasser'),
+('ride', 'rode', 'ridden', 'monter (vélo, cheval)'),
+('ring', 'rang', 'rung', 'sonner / téléphoner'),
+('rise', 'rose', 'risen', 'lever'),
+('run', 'ran', 'run', 'courir'),
+('saw', 'saw / sawed', 'sawn / sawed', 'scier'),
+('say', 'said', 'said', 'dire'),
+('see', 'saw', 'seen', 'voir'),
+('seek', 'sought', 'sought', 'chercher'),
+('sell', 'sold', 'sold', 'vendre'),
+('send', 'sent', 'sent', 'envoyer'),
+('set', 'set', 'set', 'fixer'),
+('shake', 'shook', 'shaken', 'secouer'),
+('shed', 'shed', 'shed', 'répandre / laisser tomber'),
+('shine', 'shone', 'shone', 'briller'),
+('shoe', 'shod', 'shod', 'chausser'),
+('shoot', 'shot', 'shot', 'tirer / fusiller'),
+('show', 'showed', 'shown', 'montrer'),
+('shut', 'shut', 'shut', 'fermer'),
+('sing', 'sang', 'sung', 'chanter'),
+('sink', 'sank / sunk', 'sunk / sunken', 'couler'),
+('sit', 'sat', 'sat', 's''asseoir'),
+('slay', 'slew', 'slain', 'tuer'),
+('sleep', 'slept', 'slept', 'dormir'),
+('slide', 'slid', 'slid', 'glisser'),
+('slink', 'slunk / slinked', 'slunk / slinked', 's''en aller furtivement'),
+('slit', 'slit', 'slit', 'fendre'),
+('smell', 'smelt', 'smelt', 'sentir'),
+('sow', 'sowed', 'sown / sowed', 'semer'),
+('speak', 'spoke', 'spoken', 'parler'),
+('speed', 'sped', 'sped', 'aller vite'),
+('spell', 'spelt', 'spelt', 'épeler / orthographier'),
+('spend', 'spent', 'spent', 'dépenser / passer du temps'),
+('spill', 'spilt / spilled', 'spilt / spilled', 'renverser'),
+('spin', 'spun', 'spun', 'tourner / faire tourner'),
+('spit', 'spat / spit', 'spat / spit', 'cracher'),
+('split', 'split', 'split', 'fendre'),
+('spoil', 'spoilt', 'spoilt', 'gâcher / gâter'),
+('spread', 'spread', 'spread', 'répandre'),
+('spring', 'sprang', 'sprung', 'surgir / jaillir / bondir'),
+('stand', 'stood', 'stood', 'être debout'),
+('steal', 'stole', 'stolen', 'voler / dérober'),
+('stick', 'stuck', 'stuck', 'coller'),
+('sting', 'stung', 'stung', 'piquer'),
+('stink', 'stank', 'stunk', 'puer'),
+('strew', 'strewed', 'strewn / strewed', 'éparpiller'),
+('strike', 'struck', 'stricken / struck', 'frapper'),
+('strive', 'strove', 'striven', 's''efforcer'),
+('swear', 'swore', 'sworn', 'jurer'),
+('sweat', 'sweat / sweated', 'sweat / sweated', 'suer'),
+('sweep', 'swept', 'swept', 'balayer'),
+('swell', 'swelled', 'swollen / swelled', 'gonfler / enfler'),
+('swim', 'swam', 'swum', 'nager'),
+('swing', 'swung', 'swung', 'se balancer'),
+('take', 'took', 'taken', 'prendre'),
+('teach', 'taught', 'taught', 'enseigner'),
+('tear', 'tore', 'torn', 'déchirer'),
+('tell', 'told', 'told', 'dire / raconter'),
+('think', 'thought', 'thought', 'penser'),
+('thrive', 'throve / thrived', 'thriven / thrived', 'prospérer'),
+('throw', 'threw', 'thrown', 'jeter'),
+('thrust', 'thrust', 'thrust', 'enfoncer'),
+('tread', 'trod', 'trodden', 'piétiner quelque chose'),
+('typeset', 'typeset', 'typeset', 'composer'),
+('undergo', 'underwent', 'undergone', 'subir'),
+('understand', 'understood', 'understood', 'comprendre'),
+('wake', 'woke', 'woken', 'réveiller'),
+('wear', 'wore', 'worn', 'porter (avoir sur soi)'),
+('weep', 'wept', 'wept', 'pleurer'),
+('wet', 'wet / wetted', 'wet / wetted', 'mouiller'),
+('win', 'won', 'won', 'gagner'),
+('wind', 'wound', 'wound', 'enrouler / remonter'),
+('withdraw', 'withdrew', 'withdrawn', 'se retirer'),
+('wring', 'wrung', 'wrung', 'tordre'),
+('write', 'wrote', 'written', 'écrire');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `User`
 --
 
@@ -3063,14 +3258,15 @@ CREATE TABLE IF NOT EXISTS `User` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `User`
 --
 
 INSERT INTO `User` (`id`, `name`) VALUES
-(1, 'Bennette');
+(1, 'Bennette'),
+(2, 'amandine');
 
 -- --------------------------------------------------------
 
@@ -3085,7 +3281,7 @@ CREATE TABLE IF NOT EXISTS `UserGameResult` (
   `difficultyId` int(11) NOT NULL,
   `points` int(11) NOT NULL,
   `lang` varchar(255) NOT NULL,
-  UNIQUE KEY `userId` (`userId`,`categoryId`,`gameId`,`difficultyId`)
+  UNIQUE KEY `userId` (`userId`,`categoryId`,`gameId`,`difficultyId`,`lang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -3093,8 +3289,10 @@ CREATE TABLE IF NOT EXISTS `UserGameResult` (
 --
 
 INSERT INTO `UserGameResult` (`userId`, `categoryId`, `gameId`, `difficultyId`, `points`, `lang`) VALUES
+(1, 1, 1, 1, 1, 'fr'),
 (1, 2, 1, 3, 2, 'en'),
-(1, 3, 1, 2, 0, 'en');
+(1, 3, 1, 2, 0, 'en'),
+(2, 4, 1, 1, 1, 'en');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
